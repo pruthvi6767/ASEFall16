47ce474d9d733c286c215da81363b6a623dea74a
Pruthvi6767 <pruthvi.vkb@gmail.com>
2016-09-08 11:13:08 -0500

    Source files

--- LAB2/Source/WebApp1/wino.js
+++ LAB2/Source/WebApp1/wino.js
@@ -23,31 +23,26 @@ app.controller('MyCont', function($scope, $http) {
             .success(function (response) {
                 $scope.imgWidth = 150;
                 $scope.imgHeight = 150;
-                $scope.wtData = response.coord;
-                $scope.ct = response.name;
-                $scope.tmp = response.main.temp - 272.15;
-                $scope.minTemp = response.main.temp_min - 272.15;
-                $scope.maxTemp = response.main.temp_max - 272.15;
-                $scope.hmdy = response.main.humidity;
-                $scope.imgCode = response.weather[0].icon;
+                $scope.wtData1 = response.coord;
+                $scope.ct1 = response.name;
+                $scope.tmp1 = response.main.temp - 272.15;
+                $scope.minTemp1 = response.main.temp_min - 272.15;
+                $scope.maxTemp1 = response.main.temp_max - 272.15;
+                $scope.hmdy1 = response.main.humidity;
+                $scope.imgCode1 = response.weather[0].icon;
             });
 
+        var url1 = "http://api.openweathermap.org/data/2.5/weather?q=" +($scope.destination)+"&APPID=ae917b1257d3132947ca6e1efde11037"
+        $http.get(url1)
+            .success(function(response) {
+                $scope.wtData2 = response.coord;
+                $scope.ct2 = response.name;
+                $scope.tmp2 = response.main.temp - 272.15;
+                $scope.minTemp2 = response.main.temp_min - 272.15;
+                $scope.maxTemp2 = response.main.temp_max - 272.15;
+                $scope.hmdy2 = response.main.humidity;
+                $scope.imgCode2 = response.weather[0].icon;
+            });
     };
 
-
-    $scope.funGet= function(){
-    var url1 = "http://api.openweathermap.org/data/2.5/weather?q=" +($scope.destination)+"&APPID=ae917b1257d3132947ca6e1efde11037"
-    $http.get(url1)
-        .success(function(response) {
-            $scope.imgWidth = 150;
-            $scope.imgHeight = 150;
-            $scope.wtData = response.coord;
-            $scope.ct = response.name;
-            $scope.tmp = response.main.temp - 272.15;
-            $scope.minTemp = response.main.temp_min - 272.15;
-            $scope.maxTemp = response.main.temp_max - 272.15;
-            $scope.hmdy = response.main.humidity;
-            $scope.imgCode = response.weather[0].icon;
-        });
-    };
-});
+    });
